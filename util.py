559c51823602a9fd31168b99f87b844fe1ea558b
iyume <iyumelive@gmail.com>
2022-04-19 16:41:47 +0800

    remove semantic

--- util.py
+++ util.py
@@ -8,11 +8,9 @@ import numpy as np
 import torch
 from torch import Tensor, nn
 from torch.utils.data import Dataset as BaseDataset
-from torchvision import transforms
 from torchvision.transforms import ToTensor
 
 from config import config
-from models.bisenet import BiSeNetV2
 
 logging.basicConfig(
     level=(
@@ -25,43 +23,6 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
-bisenn = BiSeNetV2(19, aux_mode="pred")
-bisenn.load_state_dict(
-    torch.load("./models/model_final_v2_city.pth", map_location="cpu"), strict=False
-)
-bisenn.eval()
-bisenn.to(config.device)
-
-bisenn_norm = transforms.Compose(
-    [transforms.Normalize(mean=(0.3257, 0.3690, 0.3223), std=(0.2112, 0.2148, 0.2115))]
-)
-
-# color map respect to https://github.com/mcordts/cityscapesScripts/blob/master/cityscapesscripts/helpers/labels.py
-color_semantic = torch.tensor(
-    [
-        (128, 64, 128),  # 0: road
-        (244, 35, 232),  # 1: sidewalk
-        (70, 70, 70),  # 2: building
-        (102, 102, 156),  # 3: wall
-        (190, 153, 153),  # 4: fence
-        (153, 153, 153),  # 5: pole
-        (250, 170, 30),  # 6: traffic light
-        (220, 220, 0),  # 7: traffic sign
-        (107, 142, 35),  # 8: vegetation
-        (152, 251, 152),  # 9: terrain
-        (70, 130, 180),  # 10: sky
-        (220, 20, 60),  # 11: person
-        (255, 0, 0),  # 12: rider
-        (0, 0, 142),  # 13: car
-        (0, 0, 70),  # 14: truck
-        (0, 60, 100),  # 15: bus
-        (0, 80, 100),  # 16: train
-        (0, 0, 230),  # 17: motorcycle
-        (119, 11, 32),  # 18: bicycle
-    ]
-)
-
-
 def imsave_tensor(filename: str, x: Tensor) -> None:
     """Save (C,H,W) tensor."""
     im = x.squeeze().detach().numpy() * 255
@@ -71,40 +32,6 @@ def imsave_tensor(filename: str, x: Tensor) -> None:
         raise ValueError
     im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
     cv2.imwrite(str(Path("output") / filename), im)
-
-
-def get_semantic_color_map(x: Tensor) -> Tensor:
-    """
-    Args:
-        x: shape (N,C,H,W)
-
-    Return:
-        (N,H,W,C)
-    """
-    if len(x.shape) == 3:
-        x = x.unsqueeze(0)
-    if x.shape[1] == 1:
-        x = torch.repeat_interleave(x, 3, dim=1)  # requires torch 1.1.0+
-    index_map = bisenn(bisenn_norm(x))
-    semantic_map = color_semantic[index_map]
-    return semantic_map
-
-
-def create_label(vi: Tensor, ir: Tensor) -> Tensor:
-    """
-    Args:
-        vi: shape (N,C,H,W), C=1
-        ir: shape (N,C,H,W), C=1
-    """
-    ir3c = torch.cat((ir, ir, ir), dim=1)
-    semantic_map = bisenn(
-        torch.cat(tuple(bisenn_norm(x).unsqueeze(0) for x in ir3c), dim=0)
-    )  # (N,H,W) store labels
-    label = vi.clone()
-    for i in range(semantic_map.shape[0]):  # iter batch size
-        mask = (semantic_map[i] == 11) + (semantic_map[i] == 13)
-        label[i][:, mask] = ir[i][:, mask]
-    return label
 
 
 class MSRSset(BaseDataset):
